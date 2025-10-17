480c6e4a881f11e7c79fb8f2c1486ef83b96ff47
Spencer, Jonathan F - St Louis, MO <Jonathan.F.Spencer@usps.gov>
2025-10-17 09:16:33 -0500

    add warning banner for USPS AzureChat discontinuation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,26 @@ import '@/styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
+
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
   return (
     <div className={inter.className}>
+      {/* Warning Banner */}
+      <div style={{
+        background: '#fff3cd',
+        color: '#856404',
+        padding: '12px',
+        textAlign: 'center',
+        borderBottom: '1px solid #ffeeba',
+        fontWeight: 'bold',
+        fontSize: '1rem',
+        zIndex: 9999,
+        position: 'relative'
+      }}>
+        ATTENTION: USPS AzureChat will be discontinued as of November 1, 2025. Please use DaiSE instead of AzureChat. For more information about DaiSE, see <a href="https://daise.usps.gov/faq" target="_blank" rel="noopener noreferrer" style={{ color: '#856404', textDecoration: 'underline' }}>https://daise.usps.gov/faq</a>.
+      </div>
       <Toaster />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
